72f7ab84d9ab0438a00f15230f933e9ef5b1f8a3
Derek <derek.feng@gmail.com>
2021-12-23 22:59:35 +0000

    add rehypePlugins to gatsby-util.js

--- theme/gatsby-util.js
+++ theme/gatsby-util.js
@@ -8,6 +8,7 @@ const defaultOptions = (pluginOptions) => {
 
       mdxOtherwiseConfigured: false, // advanced, use your own mdx plugin config, See https://github.com/theowenyoung/gatsby-theme-primer-wiki/blob/main/theme/gatsby-config.js#L31-L67
       remarkPlugins: [], // add gatsby-plugin-mdx remarkPlugins
+      rehypePlugins: [], // add gatsby-plugin-mdx rehypePlugins
       gatsbyRemarkPlugins: [], // add gatsby-plugin-mdx gatsbyRemarkPlugins
       extensions: [`.mdx`, ".md", ".markdown"], // supported file extensions for mdx
       sidebarDefault: "auto", // first summary -> tags -> files tree , value can be auto, summary, tag, category
