b909588f12988805d1dee0dfc65800e3d6f5c54e
Owen Young <theowenyoung@gmail.com>
2022-03-08 13:55:47 +0800

    docs(changeset): change blockquote style

--- theme/src/components/blockquote.js
+++ theme/src/components/blockquote.js
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 import themeGet from "@styled-system/theme-get";
+//   color: ${themeGet("colors.auto.gray.5")};
 
 const Blockquote = styled.blockquote`
-  margin: 0 0 ${themeGet("space.3")};
+  margin: 0 0 ${themeGet("space.5")} 0;
   padding: 0 ${themeGet("space.3")};
-  color: ${themeGet("colors.auto.gray.5")};
-  border-left: 0.25em solid ${themeGet("colors.auto.gray.2")};
-
+  border-left: 0.25em solid ${themeGet("colors.auto.gray.3")};
+  color: ${themeGet("colors.auto.gray.7")};
   > :first-child {
     margin-top: 0;
   }
